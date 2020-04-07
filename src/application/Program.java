d1f8285142f943e1cea12e6a571952c6ca1f6ac6
David Chaves <davi.ch.fe@gmail.com>
2020-04-07 17:24:37 -0300

    findById implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -11,13 +11,12 @@ public class Program {
 
 	public static void main(String[] args) {
 		
-		Department dp = new Department(1, "Books");
-		
-		Seller s = new Seller(21, "Bob", "bobBrown@gmail.com", new Date(), 3000.0, dp);
-		
+
 		SellerDao sld = DaoFactory.createSellerDao();
 		
-		System.out.print(s);
+		Seller seller = sld.findById(3);
+		
+		System.out.print(seller);
 
 	}
 
--- src/model/dao/impl/SellerDaoJDBC.java
+++ src/model/dao/impl/SellerDaoJDBC.java
@@ -1,11 +1,24 @@
 package model.dao.impl;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.List;
 
+import db.DbException;
 import model.dao.SellerDao;
+import model.entities.Department;
 import model.entities.Seller;
 
 public class SellerDaoJDBC implements SellerDao{
+	
+	private Connection conn = null;
+	
+	
+	public SellerDaoJDBC(Connection conn) {
+		this.conn = conn;
+	}
 
 	@Override
 	public void insert(Seller obj) {
@@ -26,9 +39,36 @@ public class SellerDaoJDBC implements SellerDao{
 	}
 
 	@Override
-	public SellerDao findById(Integer id) {
-		// TODO Auto-generated method stub
-		return null;
+	public Seller findById(Integer id) {
+		PreparedStatement st = null;
+		ResultSet rs = null;
+		try {
+			st = conn.prepareStatement("select s.*, d.name as 'DepName' from seller s "
+					+ "inner join department d on d.id = s.departmentId "
+					+ "where d.id = ? ");
+			st.setInt(1, id);
+			
+			rs = st.executeQuery();
+			
+			if(rs.next()) {
+				Department dp = new Department();
+				dp.setId(rs.getInt("DepartmentId"));
+				dp.setName(rs.getString("DepName"));
+				Seller s = new Seller();
+				s.setId(rs.getInt("Id"));
+				s.setName(rs.getString("Name"));
+				s.setEmail(rs.getString("Email"));
+				s.setBirthDate(rs.getDate("BirthDate"));
+				s.setDepartment(dp);
+				
+				return s;
+			}
+			
+			return null;
+			
+		}catch(SQLException e) {
+			throw new DbException(e.getMessage());
+		}
 	}
 
 	@Override
