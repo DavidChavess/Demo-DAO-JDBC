7115fcb710d400b030d3cd22c6e0bae9763a0356
David Chaves <davi.ch.fe@gmail.com>
2020-04-07 13:22:53 -0300

    SellerDaoJDBC and DaoFactory

--- src/application/Program.java
+++ src/application/Program.java
@@ -2,6 +2,8 @@ package application;
 
 import java.util.Date;
 
+import model.dao.DaoFactory;
+import model.dao.SellerDao;
 import model.entities.Department;
 import model.entities.Seller;
 
@@ -11,8 +13,9 @@ public class Program {
 		
 		Department dp = new Department(1, "Books");
 		
-		
 		Seller s = new Seller(21, "Bob", "bobBrown@gmail.com", new Date(), 3000.0, dp);
+		
+		SellerDao sld = DaoFactory.createSellerDao();
 		
 		System.out.print(s);
 
