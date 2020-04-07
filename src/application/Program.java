98a682b08fd0dcc48c600978911551f68802f876
David Chaves <davi.ch.fe@gmail.com>
2020-04-07 18:43:19 -0300

    insert implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,5 +1,6 @@
 package application;
 
+import java.util.Date;
 import java.util.List;
 
 import model.dao.DaoFactory;
@@ -12,27 +13,30 @@ public class Program {
 	public static void main(String[] args) {
 		
 
-		SellerDao sld = DaoFactory.createSellerDao();
+		SellerDao sellerDao = DaoFactory.createSellerDao();
 		
-		Seller seller = sld.findById(3);
+		Seller seller = sellerDao.findById(3);
 		System.out.println("===Test01 findById===");
 		System.out.print(seller);
 		
 		
 		System.out.println("\n===Test02 findByDepartment===");
 		Department department = new Department(2, null);
-		List<Seller> departments = sld.findByDepartment(department);
+		List<Seller> departments = sellerDao.findByDepartment(department);
 		departments.forEach(System.out::println);
 		
 		
 		System.out.println("\n\n ===Test03 findAll===");
-		List<Seller> listAll = sld.findAll();
+		List<Seller> listAll = sellerDao.findAll();
 		listAll.forEach(System.out::println);
 		
 		
-
+	
 		
-		
+		System.out.println("\n\n ===Test04 Insert===");
+		Seller insertSeller = new Seller(null, "Greg", "greg@gmail.com", new Date(), 4000.0, department);
+		sellerDao.insert(insertSeller);
+		System.out.println("INSERTED! NEW ID "+ insertSeller.getId());
 
 	}
 
--- src/model/dao/impl/SellerDaoJDBC.java
+++ src/model/dao/impl/SellerDaoJDBC.java
@@ -4,11 +4,13 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import db.DB;
 import db.DbException;
 import model.dao.SellerDao;
 import model.entities.Department;
@@ -25,8 +27,37 @@ public class SellerDaoJDBC implements SellerDao{
 
 	@Override
 	public void insert(Seller obj) {
-		// TODO Auto-generated method stub
 		
+		PreparedStatement st = null;
+		try {		
+			st = conn.prepareStatement(
+					"INSERT INTO seller " 
+					+ " (Name, Email, BirthDate, BaseSalary, DepartmentId) "
+					+ "VALUES (? ,?, ?, ?, ?)",
+					Statement.RETURN_GENERATED_KEYS);
+					
+					st.setString(1, obj.getName());
+					st.setString(2, obj.getEmail());
+					st.setDate(3, new java.sql.Date(obj.getBirthDate().getTime()));
+					st.setDouble(4, obj.getBaseSalary());
+					st.setInt(5, obj.getDepartment().getId());	
+					
+			int rowsAffected = st.executeUpdate();
+			
+			if(rowsAffected > 0) {
+				ResultSet rs = st.getGeneratedKeys();
+				if(rs.next()) {
+					obj.setId(rs.getInt(1));
+				}
+				DB.closeResultSet(rs);
+			}else {
+				throw new DbException("Unexpected error! No affected rows");
+			}
+		}catch(SQLException e ) {
+			throw new DbException(e.getMessage());
+		}finally {
+			DB.closeStatement(st);
+		}
 	}
 
 	@Override
