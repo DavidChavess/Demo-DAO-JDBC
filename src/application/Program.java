7696a04c5299a3385ab86f3273c1c5431811a84e
David Chaves <davi.ch.fe@gmail.com>
2020-04-07 12:59:18 -0300

    Seller Entity Class

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,6 +1,9 @@
 package application;
 
+import java.util.Date;
+
 import model.entities.Department;
+import model.entities.Seller;
 
 public class Program {
 
@@ -8,7 +11,10 @@ public class Program {
 		
 		Department dp = new Department(1, "Books");
 		
-		System.out.print(dp);
+		
+		Seller s = new Seller(21, "Bob", "bobBrown@gmail.com", new Date(), 3000.0, dp);
+		
+		System.out.print(s);
 
 	}
 
